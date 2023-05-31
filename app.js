02f67a3d33a889ad43ba7ed8d5730ad509f0973b
andrefaca <andre@andrefaca.com>
2023-05-31 18:00:25 +0100

    Files Changed

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT;
-
-require('dotenv').config();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
